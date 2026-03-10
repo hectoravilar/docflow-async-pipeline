306b2a369346e863834d7586d656e3694d89b156
Hector Avila <hectoravllr@gmail.com>
2026-03-10 09:46:31 -0300

    docs: documentando index.py

--- src/lambda-CreateS3File/index.py
+++ src/lambda-CreateS3File/index.py
@@ -1,23 +1,33 @@
-"""
-Function name: CreateS3File
-Runner: AWS Lambda
-Handler: lambda_handler
-Language: Python3.12
-Variables: bucket_name
+"""Lambda para criar arquivo diário no S3.
 
-Description:
-This is an AWS Lambda function to create an File in the S3 Bucket.
-The file name should be the current time and date, following UTC.s
+Esta função é acionada pelo EventBridge às 10:00am diariamente
+para inserir um arquivo no bucket S3 com timestamp da execução.
 """
+
 import boto3
 import os
 from datetime import datetime
 
 def lambda_handler(event, context):
+    """Handler principal da função Lambda.
+    
+    Args:
+        event: Evento recebido do EventBridge
+        context: Contexto de execução da Lambda
+        
+    Returns:
+        dict: Resposta com statusCode e mensagem de sucesso
+    """
+    # Inicializa cliente S3
     s3 = boto3.client('s3')
+    
+    # Obtém nome do bucket da variável de ambiente
     bucket_name = os.environ['bucket_name']
+    
+    # Gera nome do arquivo com data/hora da execução
     filename = datetime.utcnow().strftime('%Y-%m-%d_%H-%M-%S.txt')
     
+    # Cria arquivo no S3 com conteúdo contendo timestamp
     s3.put_object(
         Bucket=bucket_name,
         Key=filename,
