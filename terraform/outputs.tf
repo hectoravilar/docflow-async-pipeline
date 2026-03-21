adb16dfed5a47d8ae75b12becf01b150d5de45a0
Hector Avila <hectoravllr@gmail.com>
2026-03-20 23:43:59 -0300

    feat: add outputs.tf and wire variables into main terraform config

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,13 @@
+output "s3_bucket_name" {
+  description = "Name of the S3 Bucket"
+  value       = aws_s3_bucket.docflow_bucket.bucket
+}
 
+output "sqs_queue" {
+  description = "URL of the SQS queue"
+  value       = aws_sqs_queue.docflow_queue.url
+}
+output "dynamodb_table_name" {
+  description = "Name of the DynamoDB table"
+  value       = aws_dynamodb_table.docflow_table.name
+}
