7cada7d49d11874307b4ff0ac52db2855c755c8e
Hector Avila <hectoravllr@gmail.com>
2026-03-26 16:32:45 -0300

    feat(api-gateway): grant execution permission to lambda
    - Add resource-based policy to allow API Gateway to invoke Lambda
    - Expose final API endpoint URL via Terraform outputs

--- terraform/api.tf
+++ terraform/api.tf
@@ -84,3 +84,10 @@ resource "aws_apigatewayv2_route" "lambda_route" {
   route_key = "POST /upload"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api_handler.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/*"
+}
