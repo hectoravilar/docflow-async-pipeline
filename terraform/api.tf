6a51d5cdd28f6807b4f78c11542e3daf49699041
Hector Avila <hectoravllr@gmail.com>
2026-03-26 10:51:35 -0300

    feat(infra): add lambda archive and base iam role for api

---
+++ terraform/api.tf
@@ -0,0 +1,23 @@
+# Archive a single file.
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/../lambda/lambda_function.py"
+  output_path = "${path.module}/lambda_function.zip"
+}
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = lower("${var.project_name}-lambda-role-${var.environment}")
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+
